8d7d1c54dc4d4da802ff10c66b1902607a4ec810
Warren He <w@oasislabs.com>
2021-01-08 16:01:10 -0800

    (unfinished) set up custom serialization

--- client-sdk/ts-web/src/index.js
+++ client-sdk/ts-web/src/index.js
@@ -1,20 +1,35 @@
+const cbor = require('cbor-js');
 const grpc = {};
 grpc.web = require('grpc-web');
+
+function serializeRequestCBOR(req) {
+    return new Uint8Array(cbor.encode(req));
+}
+
+function deserializeResponseCBOR(u8) {
+    let buf;
+    if (u8.byteOffset === 0 && u8.byteLength === u8.buffer.byteLength) {
+        buf = u8.buffer;
+    } else {
+        buf = u8.buffer.slice(u8.byteOffset, u8.byteOffset + u8.byteLength);
+    }
+    return cbor.decode(buf);
+}
 
 const statusMD = new grpc.web.MethodDescriptor(
     '/oasis-core.NodeController/GetStatus',
     grpc.web.MethodType.UNARY,
     Object,
     Object,
-    (v) => new Uint8Array([0xf6]),
-    (v) => v
+    serializeRequestCBOR,
+    deserializeResponseCBOR
 );
 
 const base = 'http://localhost:42280';
 const oc = new grpc.web.GrpcWebClientBase();
 
 function status(request) {
-    return oc.unaryCall(base + statusMD.name, request, {}, statusMD);
+    return oc.unaryCall(base + statusMD.name, request, null, statusMD);
 }
 
 (async function () {
