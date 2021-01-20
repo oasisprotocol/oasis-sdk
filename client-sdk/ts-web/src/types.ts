310c68a5cdde1c052f6f52727464cada2861141c
Warren He <w@oasislabs.com>
2021-01-19 16:33:21 -0800

    ts-web: add consensus transactions with results type

--- client-sdk/ts-web/src/types.ts
+++ client-sdk/ts-web/src/types.ts
@@ -1,5 +1,10 @@
 export type NotModeled = Object;
 
+export interface ConsensusTransactionsWithResults extends Map<string, any> {
+    get(key: 'transactions'): Uint8Array[];
+    get(key: 'results'): NotModeled[];
+}
+
 export interface StakingDelegation extends Map<string, any> {
     get(key: 'shares'): Uint8Array;
 }
