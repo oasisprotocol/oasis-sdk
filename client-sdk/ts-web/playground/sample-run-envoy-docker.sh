1bea04647f5a5f0ad2bd1882f0b9ed258dcf5653
Warren He <w@oasislabs.com>
2021-02-03 11:20:33 -0800

    ts-web: update development runner to latest testnet

--- client-sdk/ts-web/playground/sample-run-envoy-docker.sh
+++ client-sdk/ts-web/playground/sample-run-envoy-docker.sh
@@ -7,7 +7,7 @@ docker run \
     -e ENVOY_GID=1000 \
     --network host \
     -v "$PWD/sample-envoy.yaml:/mnt/ts-web/sample-envoy.yaml" \
-    -v "$PWD/../../../../testnet/testnet-b/serverdir/node/internal.sock:/mnt/ts-web/node/internal.sock" \
+    -v "$PWD/../../../../testnet/testnet-c/serverdir/node/internal.sock:/mnt/ts-web/node/internal.sock" \
     -w /mnt/ts-web \
     envoyproxy/envoy:v1.16-latest \
     -c sample-envoy.yaml
