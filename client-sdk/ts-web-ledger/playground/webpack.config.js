54cf83ff33d2ec46ce0cf8539717467b11dd4a70
Warren He <w@oasislabs.com>
2021-02-08 16:42:57 -0800

    ts-web: separate tsc build step

--- client-sdk/ts-web-ledger/playground/webpack.config.js
+++ client-sdk/ts-web-ledger/playground/webpack.config.js
@@ -1,20 +1,5 @@
 module.exports = {
     mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [
-            '.ts',
-            '.js',
-        ],
-    },
     devServer: {
         contentBase: 'dist',
     },
