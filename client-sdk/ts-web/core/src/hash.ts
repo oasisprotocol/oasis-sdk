10f521b7d4a9a80b897c2c41ac154f40dd2eccd1
Nick Hynes <nhynes@oasislabs.com>
2021-03-06 18:05:11 +0300

    Use WebCrypto for hashing

--- client-sdk/ts-web/core/src/hash.ts
+++ client-sdk/ts-web/core/src/hash.ts
@@ -1,5 +1,9 @@
-import {sha512_256} from 'js-sha512';
-
+/** Returns the SHA-512/256 hash of `data`. */
 export async function hash(data: Uint8Array) {
-    return new Uint8Array(sha512_256.arrayBuffer(data));
+    const dataBuf = data.buffer.slice(
+        data.byteOffset,
+        data.byteOffset + data.byteLength
+    );
+    const hashBuf = await crypto.subtle.digest("SHA-512", dataBuf);
+    return new Uint8Array(hashBuf, 0, 256 / 8);
 }
