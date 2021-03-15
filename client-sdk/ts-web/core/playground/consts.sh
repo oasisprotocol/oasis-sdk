04e82e9cfd071d7f1a9d3297da6a814ffa1069c8
Warren He <w@oasislabs.com>
2021-03-15 15:49:33 -0700

    ts-web/core: run envoy without docker

--- client-sdk/ts-web/core/playground/consts.sh
+++ client-sdk/ts-web/core/playground/consts.sh
@@ -1,4 +1,3 @@
 BUILD_NUMBER=3935
 OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/3935/jobs/f3463f8c-2d2c-4381-a115-5d1f34b9ff44/artifacts/b4fa89d0-2712-4591-a5d5-e70efea9db3a
 OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/3935/jobs/f3463f8c-2d2c-4381-a115-5d1f34b9ff44/artifacts/bdfa13dc-d111-4860-9823-a53cea0cb218
-ENVOY_DOCKER_IMAGE=envoyproxy/envoy:v1.16-latest
--- client-sdk/ts-web/core/playground/pull-images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eux
-. ./consts.sh
-
-docker run --rm "$ENVOY_DOCKER_IMAGE" --help
