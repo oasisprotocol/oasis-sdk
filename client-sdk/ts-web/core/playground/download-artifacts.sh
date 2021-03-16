ee581fae5eec86e0d6a272944341195173dc8979
Warren He <w@oasislabs.com>
2021-03-16 13:48:45 -0700

    ts-web/core: change a script over to curl
    No offense to wget.

--- client-sdk/ts-web/core/playground/download-artifacts.sh
+++ client-sdk/ts-web/core/playground/download-artifacts.sh
@@ -3,10 +3,10 @@
 
 mkdir -p untracked
 if [ ! -e "untracked/oasis-node-$BUILD_NUMBER" ]; then
-    wget -O "untracked/oasis-node-$BUILD_NUMBER" "$OASIS_NODE_ARTIFACT"
+    curl -fLo "untracked/oasis-node-$BUILD_NUMBER" "$OASIS_NODE_ARTIFACT"
     chmod +x "untracked/oasis-node-$BUILD_NUMBER"
 fi
 if [ ! -e "untracked/oasis-net-runner-$BUILD_NUMBER" ]; then
-    wget -O "untracked/oasis-net-runner-$BUILD_NUMBER" "$OASIS_NET_RUNNER_ARTIFACT"
+    curl -fLo "untracked/oasis-net-runner-$BUILD_NUMBER" "$OASIS_NET_RUNNER_ARTIFACT"
     chmod +x "untracked/oasis-net-runner-$BUILD_NUMBER"
 fi
