90a582bbfb635a7500012d37a3734bd00a028bb4
Warren He <w@oasislabs.com>
2021-03-16 17:20:03 -0700

    ts-web/core: bump artifacts

--- client-sdk/ts-web/core/playground/consts.sh
+++ client-sdk/ts-web/core/playground/consts.sh
@@ -1,3 +1,3 @@
-BUILD_NUMBER=3935
-OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/3935/jobs/f3463f8c-2d2c-4381-a115-5d1f34b9ff44/artifacts/b4fa89d0-2712-4591-a5d5-e70efea9db3a
-OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/3935/jobs/f3463f8c-2d2c-4381-a115-5d1f34b9ff44/artifacts/bdfa13dc-d111-4860-9823-a53cea0cb218
+BUILD_NUMBER=4079
+OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4079/jobs/5af38a50-1f03-4b8a-a715-6fa3ef132704/artifacts/13c93842-c42b-488d-b8f3-afec6d69508c
+OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4079/jobs/5af38a50-1f03-4b8a-a715-6fa3ef132704/artifacts/072c15dd-1851-46f0-93f1-e98d46325dc2
