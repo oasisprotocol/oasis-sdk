7be79b58175f83402bbc6e0c81c8abe950741567
Warren He <w@oasislabs.com>
2021-03-18 17:04:54 -0700

    ts-web/core: get artifacts from release

--- client-sdk/ts-web/core/playground/consts.sh
+++ client-sdk/ts-web/core/playground/consts.sh
@@ -1,3 +1,1 @@
-BUILD_NUMBER=4236
-OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4236/jobs/1888653a-a5ec-4f78-ab07-49ba697ca76a/artifacts/94c5fb55-faf5-4ab7-b209-37d4b955fcad
-OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4236/jobs/1888653a-a5ec-4f78-ab07-49ba697ca76a/artifacts/54d56fd1-3ec8-48ab-9d65-d96e72d21660
+OASIS_CORE_VERSION=21.0
--- client-sdk/ts-web/core/playground/download-artifacts.sh
+++ client-sdk/ts-web/core/playground/download-artifacts.sh
@@ -2,11 +2,12 @@
 . ./consts.sh
 
 mkdir -p untracked
-if [ ! -e "untracked/oasis-node-$BUILD_NUMBER" ]; then
-    curl -fLo "untracked/oasis-node-$BUILD_NUMBER" "$OASIS_NODE_ARTIFACT"
-    chmod +x "untracked/oasis-node-$BUILD_NUMBER"
-fi
-if [ ! -e "untracked/oasis-net-runner-$BUILD_NUMBER" ]; then
-    curl -fLo "untracked/oasis-net-runner-$BUILD_NUMBER" "$OASIS_NET_RUNNER_ARTIFACT"
-    chmod +x "untracked/oasis-net-runner-$BUILD_NUMBER"
+if [ ! -e "untracked/oasis_core_${OASIS_CORE_VERSION}_linux_amd64/oasis-node" ]; then
+    (
+        cd untracked
+        curl -fLO "https://github.com/oasisprotocol/oasis-core/releases/download/v$OASIS_CORE_VERSION/oasis_core_${OASIS_CORE_VERSION}_linux_amd64.tar.gz"
+        tar -xf "oasis_core_${OASIS_CORE_VERSION}_linux_amd64.tar.gz" \
+            "oasis_core_${OASIS_CORE_VERSION}_linux_amd64/oasis-node" \
+            "oasis_core_${OASIS_CORE_VERSION}_linux_amd64/oasis-net-runner"
+    )
 fi
