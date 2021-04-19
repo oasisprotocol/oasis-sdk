77e29ebae6e61885d7924c6292680a4f96180916
pro-wh <w@oasislabs.com>
2021-04-19 14:30:41 -0700

    runtime-sdk: review suggestions
    Co-authored-by: Nick Hynes <nhynes@oasislabs.com>

--- runtime-sdk/src/modules/core/test.rs
+++ runtime-sdk/src/modules/core/test.rs
@@ -4,25 +4,24 @@ use super::{Module as Core, API as _};
 
 #[test]
 fn test_use_gas() {
+    const MAX_GAS: u64 = 1000;
     let mut mock = mock::Mock::default();
     let mut ctx = mock.create_ctx();
     let mut tx = mock::transaction();
-    tx.auth_info.fee.gas = 1000;
+    tx.auth_info.fee.gas = MAX_GAS;
 
     ctx.with_tx(tx.clone(), |mut tx_ctx, _call| {
-        Core::use_gas(&mut tx_ctx, 100)
-            .expect("using gas under limit should succeed");
+        Core::use_gas(&mut tx_ctx, MAX_GAS).expect("using gas under limit should succeed");
     });
 
     ctx.with_tx(tx.clone(), |mut tx_ctx, _call| {
-        Core::use_gas(&mut tx_ctx, 950)
+        Core::use_gas(&mut tx_ctx, MAX_GAS)
             .expect("gas across separate transactions shouldn't accumulate");
     });
 
     ctx.with_tx(tx.clone(), |mut tx_ctx, _call| {
-        Core::use_gas(&mut tx_ctx, 600).unwrap();
-        Core::use_gas(&mut tx_ctx, 600)
-            .expect_err("gas in same transaction should accumulate");
+        Core::use_gas(&mut tx_ctx, MAX_GAS).unwrap();
+        Core::use_gas(&mut tx_ctx, 1).expect_err("gas in same transaction should accumulate");
     });
 
     ctx.with_tx(tx.clone(), |mut tx_ctx, _call| {
