781db209761987dd9dd1c7991c984d21596a5ea3
Warren He <w@oasislabs.com>
2021-04-30 14:44:11 -0700

    modules/consensus_accounts: add gas costs for transactions

--- tests/runtimes/simple-consensus/src/lib.rs
+++ tests/runtimes/simple-consensus/src/lib.rs
@@ -10,9 +10,25 @@ impl sdk::Runtime for Runtime {
     type Modules = (
         modules::accounts::Module,
         modules::consensus_accounts::Module<modules::accounts::Module, modules::consensus::Module>,
+        modules::core::Module,
     );
 
     fn genesis_state() -> <Self::Modules as sdk::module::MigrationHandler>::Genesis {
-        Default::default()
+        (
+            Default::default(),
+            modules::consensus_accounts::Genesis {
+                parameters: modules::consensus_accounts::Parameters {
+                    gas_costs: modules::consensus_accounts::GasCosts {
+                        tx_deposit: 100,
+                        tx_withdraw: 100,
+                    },
+                },
+            },
+            modules::core::Genesis {
+                parameters: modules::core::Parameters {
+                    max_batch_gas: 10_000,
+                },
+            },
+        )
     }
 }
