2e7859873d534fc1836ba075bbe5a506bcb6f541
Warren He <w@oasislabs.com>
2021-05-19 16:55:19 -0700

    runtime-sdk: actually call the approve_unverified_tx step

---
+++ tests/runtimes/simple-keyvalue/src/test.rs
@@ -0,0 +1,33 @@
+use oasis_runtime_sdk::{
+    module::AuthHandler as _,
+    modules::{core, core::Module as Core},
+    testing::mock,
+    types::transaction,
+    Context as _, Module as _,
+};
+
+#[test]
+fn test_impl_for_tuple() {
+    let mut mock = mock::Mock::default();
+    let mut ctx = mock.create_ctx();
+    Core::set_params(
+        ctx.runtime_state(),
+        &core::Parameters {
+            max_batch_gas: u64::MAX,
+            max_tx_signers: 1,
+            max_multisig_signers: 1,
+        },
+    );
+    let dummy_bytes = b"you look, you die".to_vec();
+    <super::Runtime as oasis_runtime_sdk::Runtime>::Modules::approve_unverified_tx(
+        &mut ctx,
+        &transaction::UnverifiedTransaction(
+            dummy_bytes.clone(),
+            vec![
+                transaction::AuthProof::Signature(dummy_bytes.clone().into()),
+                transaction::AuthProof::Signature(dummy_bytes.clone().into()),
+            ],
+        ),
+    )
+    .expect_err("too many authentication slots");
+}
