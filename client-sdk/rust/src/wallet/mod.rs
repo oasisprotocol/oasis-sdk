a69b30c3bf8a703212dcd5febc545a93d8158101
Nick Hynes <nhynes@oasislabs.com>
2021-06-17 15:09:08 -0500

    client-sdk/rust: add test to ed25519wallet

--- client-sdk/rust/src/wallet/mod.rs
+++ client-sdk/rust/src/wallet/mod.rs
@@ -1,12 +1,12 @@
-pub mod dummy;
 pub mod ed25519;
+pub mod nonce_provider;
 
 use anyhow::Result;
 use async_trait::async_trait;
 
 use oasis_runtime_sdk::types::transaction::{AddressSpec, AuthProof};
 
-pub type Nonce = u64;
+pub use nonce_provider::{Nonce, NonceProvider};
 
 #[async_trait]
 pub trait Wallet: Signer + NonceProvider {
@@ -16,10 +16,4 @@ pub trait Wallet: Signer + NonceProvider {
 #[async_trait]
 pub trait Signer: Send + Sync {
     async fn sign(&self, context: &[u8], message: &[u8]) -> Result<AuthProof>;
-}
-
-#[async_trait]
-pub trait NonceProvider: Send + Sync {
-    /// Fetches the next nonce. May be called several times before any transaction is submitted.
-    async fn next_nonce(&self) -> Result<Nonce>;
 }
