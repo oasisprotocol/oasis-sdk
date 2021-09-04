0a007b624d9085a8494ab7380e66eb618f8dacb5
Warren He <w@oasislabs.com>
2021-09-03 17:48:12 -0700

    ts-web/core: reflect grpc types

--- client-sdk/ts-web/core/hax-go/main.go
+++ client-sdk/ts-web/core/hax-go/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"reflect"
 	"sort"
 	"strings"
 	"time"
 
+	beacon "github.com/oasisprotocol/oasis-core/go/beacon/api"
+	"github.com/oasisprotocol/oasis-core/go/common/crypto/pvss"
+	"github.com/oasisprotocol/oasis-core/go/common/pubsub"
 	"github.com/oasisprotocol/oasis-core/go/common/quantity"
 	genesis "github.com/oasisprotocol/oasis-core/go/genesis/api"
+	governance "github.com/oasisprotocol/oasis-core/go/governance/api"
+	keymanager "github.com/oasisprotocol/oasis-core/go/keymanager/api"
+	registry "github.com/oasisprotocol/oasis-core/go/registry/api"
+	roothash "github.com/oasisprotocol/oasis-core/go/roothash/api"
+	scheduler "github.com/oasisprotocol/oasis-core/go/scheduler/api"
+	staking "github.com/oasisprotocol/oasis-core/go/staking/api"
 )
 
 type usedType struct {
@@ -20,6 +31,8 @@ var used = []*usedType{}
 var memo = map[reflect.Type]*usedType{}
 
 var prefixByPackage = map[string]string{
+	"net": "Net",
+
 	"github.com/oasisprotocol/oasis-core/go/beacon/api": "Beacon",
 	"github.com/oasisprotocol/oasis-core/go/common/cbor": "CBOR",
 	"github.com/oasisprotocol/oasis-core/go/common/crypto/signature": "Signature",
@@ -33,6 +46,8 @@ var prefixByPackage = map[string]string{
 	"github.com/oasisprotocol/oasis-core/go/keymanager/api": "KeyManager",
 	"github.com/oasisprotocol/oasis-core/go/registry/api": "Registry",
 	"github.com/oasisprotocol/oasis-core/go/roothash/api": "RootHash",
+	"github.com/oasisprotocol/oasis-core/go/roothash/api/block": "RootHash",
+	"github.com/oasisprotocol/oasis-core/go/roothash/api/commitment": "RootHash",
 	"github.com/oasisprotocol/oasis-core/go/scheduler/api": "Scheduler",
 	"github.com/oasisprotocol/oasis-core/go/staking/api": "Staking",
 	"github.com/oasisprotocol/oasis-core/go/storage/mkvs/writelog": "Storage",
@@ -41,10 +56,13 @@ var prefixByPackage = map[string]string{
 var prefixConsulted = map[string]bool{}
 
 func visitType(t reflect.Type) string {
+	_, _ = fmt.Fprintf(os.Stderr, "visiting type %v\n", t)
 	switch t {
 	case reflect.TypeOf(time.Time{}):
 		t = reflect.TypeOf(int64(0))
 	case reflect.TypeOf(quantity.Quantity{}):
+		t = reflect.TypeOf([]byte{})
+	case reflect.TypeOf(pvss.Point{}):
 		t = reflect.TypeOf([]byte{})
 	}
 	if ut, ok := memo[t]; ok {
@@ -95,6 +113,7 @@ func visitType(t reflect.Type) string {
 		mode := "object"
 		for i := 0; i < t.NumField(); i++ {
 			f := t.Field(i)
+			_, _ = fmt.Fprintf(os.Stderr, "visiting field %v\n", f)
 			if f.Anonymous {
 				if extends == "" {
 					extends = fmt.Sprintf(" extends %s", visitType(f.Type))
@@ -129,6 +148,9 @@ func visitType(t reflect.Type) string {
 			} else if jsonTag, ok := f.Tag.Lookup("json"); ok {
 				parts := strings.Split(jsonTag, ",")
 				name = parts[0]
+				if name == "-" {
+					continue
+				}
 				parts = parts[1:]
 				for _, part := range parts {
 					switch part {
@@ -140,6 +162,10 @@ func visitType(t reflect.Type) string {
 				}
 			} else {
 				name = f.Name
+			}
+			if f.PkgPath != "" {
+				// skip private fields
+				continue
 			}
 			switch mode {
 			case "object":
@@ -187,6 +213,50 @@ func visitType(t reflect.Type) string {
 	}
 }
 
+var skipMethods = map[string]bool{
+	"github.com/oasisprotocol/oasis-core/go/roothash/api.Backend.TrackRuntime": true,
+}
+
+func visitClient(t reflect.Type) {
+	_, _ = fmt.Fprintf(os.Stderr, "visiting client %v\n", t)
+	for i := 0; i < t.NumMethod(); i++ {
+		m := t.Method(i)
+		_, _ = fmt.Fprintf(os.Stderr, "visiting method %v\n", m)
+		sig := fmt.Sprintf("%s.%s.%s", t.PkgPath(), t.Name(), m.Name)
+		if skipMethods[sig] {
+			continue
+		}
+		for j := 0; j < m.Type.NumIn(); j++ {
+			u := m.Type.In(j)
+			// skip context
+			if u == reflect.TypeOf((*context.Context)(nil)).Elem() {
+				continue
+			}
+			visitType(u)
+		}
+		for j := 0; j < m.Type.NumOut(); j++ {
+			u := m.Type.Out(j)
+			// skip subscription
+			if u == reflect.TypeOf((*pubsub.Subscription)(nil)) {
+				continue
+			}
+			if u == reflect.TypeOf((*pubsub.ClosableSubscription)(nil)).Elem() {
+				continue
+			}
+			// skip error
+			if u == reflect.TypeOf((*error)(nil)).Elem() {
+				continue
+			}
+			// visit stream datum instead
+			if u.Kind() == reflect.Chan {
+				visitType(u.Elem())
+				continue
+			}
+			visitType(u)
+		}
+	}
+}
+
 func write() {
 	sort.Slice(used, func(i, j int) bool {
 		return strings.ToLower(used[i].ref) < strings.ToLower(used[j].ref)
@@ -198,6 +268,13 @@ func write() {
 
 func main() {
 	visitType(reflect.TypeOf((*genesis.Document)(nil)).Elem())
+	visitClient(reflect.TypeOf((*beacon.Backend)(nil)).Elem())
+	visitClient(reflect.TypeOf((*scheduler.Backend)(nil)).Elem())
+	visitClient(reflect.TypeOf((*registry.Backend)(nil)).Elem())
+	visitClient(reflect.TypeOf((*staking.Backend)(nil)).Elem())
+	visitClient(reflect.TypeOf((*keymanager.Backend)(nil)).Elem())
+	visitClient(reflect.TypeOf((*roothash.Backend)(nil)).Elem())
+	visitClient(reflect.TypeOf((*governance.Backend)(nil)).Elem())
 	write()
 	for prefix := range prefixByPackage {
 		if !prefixConsulted[prefix] {
