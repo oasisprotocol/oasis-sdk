a2995fddd036b1ae78ee08b7d511682b0163bd78
Warren He <w@oasislabs.com>
2021-09-16 14:20:39 -0700

    tests: add special greeting scheme

--- tests/runtimes/simple-keyvalue/src/keyvalue/types.rs
+++ tests/runtimes/simple-keyvalue/src/keyvalue/types.rs
@@ -1,5 +1,7 @@
 //! Types for the keyvalue module.
 
+use oasis_runtime_sdk::crypto::signature::{ed25519, Signature};
+
 #[derive(Clone, Debug, cbor::Encode, cbor::Decode)]
 pub struct Key {
     pub key: Vec<u8>,
@@ -9,4 +11,17 @@ pub struct Key {
 pub struct KeyValue {
     pub key: Vec<u8>,
     pub value: Vec<u8>,
+}
+
+#[derive(Clone, Debug, cbor::Encode, cbor::Decode)]
+pub struct SpecialGreetingParams {
+    pub nonce: u64,
+    pub greeting: String,
+}
+
+#[derive(Clone, Debug, cbor::Encode, cbor::Decode)]
+pub struct SpecialGreeting {
+    pub params_cbor: Vec<u8>,
+    pub from: ed25519::PublicKey,
+    pub signature: Signature,
 }
