d8c2fee73d10081b7c49bcab9732fac3eeb2ada8
Warren He <w@oasislabs.com>
2021-12-01 15:26:11 -0800

    ts-web/rt: export evm module

--- client-sdk/ts-web/rt/src/index.ts
+++ client-sdk/ts-web/rt/src/index.ts
@@ -4,6 +4,7 @@ export * as callformat from './callformat';
 export * as consensusAccounts from './consensus_accounts';
 export * as core from './core';
 export * as event from './event';
+export * as evm from './evm';
 export * as rewards from './rewards';
 export * as signatureSecp256k1 from './signature_secp256k1';
 export * as token from './token';
