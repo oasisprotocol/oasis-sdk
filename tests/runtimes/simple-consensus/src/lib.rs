8eda350a25df349615100408183fcb2956b0122f
Warren He <w@oasislabs.com>
2022-03-16 13:33:32 -0700

    modules/core: add incoming messages gas limit parameter

--- tests/runtimes/simple-consensus/src/lib.rs
+++ tests/runtimes/simple-consensus/src/lib.rs
@@ -65,6 +65,7 @@ impl sdk::Runtime for Runtime {
             modules::core::Genesis {
                 parameters: modules::core::Parameters {
                     max_batch_gas: 10_000,
+                    max_in_msgs_gas: 2_500,
                     max_tx_signers: 8,
                     max_multisig_signers: 8,
                     // These are free, in order to simplify testing.
--- tests/runtimes/simple-keyvalue/src/lib.rs
+++ tests/runtimes/simple-keyvalue/src/lib.rs
@@ -145,6 +145,7 @@ impl sdk::Runtime for Runtime {
             modules::core::Genesis {
                 parameters: modules::core::Parameters {
                     max_batch_gas: 2_000,
+                    max_in_msgs_gas: 500,
                     max_tx_signers: 8,
                     max_multisig_signers: 8,
                     gas_costs: modules::core::GasCosts {
