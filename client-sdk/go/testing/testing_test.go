c171dc8ecd5e3df34740ab24b32c78467f7229a5
Warren He <w@oasislabs.com>
2022-04-01 11:51:41 -0700

    (experimental) client-sdk/go: print testing addresses

--- client-sdk/go/testing/testing_test.go
+++ client-sdk/go/testing/testing_test.go
@@ -8,7 +8,9 @@ import (
 
 func TestPrintTestKeys(t *testing.T) {
 	fmt.Printf("A: %v\n", Alice.Signer.Public().String())
+	fmt.Printf("A addr: %s\n", Alice.Address)
 	fmt.Printf("B: %v\n", Bob.Signer.Public().String())
+	fmt.Printf("B addr: %s\n", Bob.Address)
 	fmt.Printf("C: %v\n", Charlie.Signer.Public().String())
 	fmt.Printf("D: %v\n", Dave.Signer.Public().String())
 	fmt.Printf("D(ETH): %v\n", hex.EncodeToString(Dave.EthAddress[:]))
