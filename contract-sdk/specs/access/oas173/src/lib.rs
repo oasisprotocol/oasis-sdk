25925e6add0c0f459630a5ebf220b2975d3311cf
Nick Hynes <nhynes@oasislabs.com>
2022-05-25 02:21:53 +0800

    Add ERC-173/Ownable analogue (#786)
    * contract-sdk/specs: move oas20 to token subdir
    
    * contract-sdk/specs: add Ownable contract spec

---
+++ contract-sdk/specs/access/oas173/src/lib.rs
@@ -0,0 +1,48 @@
+//! Specification of an Ownable contract.
+#![deny(rust_2018_idioms, unreachable_pub)]
+#![cfg_attr(not(test), deny(clippy::expect_used, clippy::unwrap_used))]
+#![forbid(unsafe_code)]
+
+pub mod helpers;
+#[cfg(test)]
+mod test;
+pub mod types;
+
+use oasis_contract_sdk as sdk;
+
+use types::{Error, Request, Response};
+
+struct Ownable;
+
+impl sdk::Contract for Ownable {
+    type Request = Request;
+    type Response = Response;
+    type Error = Error;
+
+    fn instantiate<C: sdk::Context>(ctx: &mut C, request: Request) -> Result<(), Error> {
+        if let Request::Instantiate = request {
+            helpers::instantiate(ctx)
+        } else {
+            Err(Error::BadRequest)
+        }
+    }
+
+    fn call<C: sdk::Context>(ctx: &mut C, request: Request) -> Result<Response, Error> {
+        Ok(match request {
+            Request::TransferOwnership { new_owner } => {
+                helpers::transfer_ownership(ctx, new_owner)?.into()
+            }
+            Request::RenounceOwnership => helpers::renounce_ownership(ctx)?.into(),
+            _ => return Err(Error::BadRequest),
+        })
+    }
+
+    fn query<C: sdk::Context>(ctx: &mut C, request: Request) -> Result<Response, Error> {
+        Ok(match request {
+            Request::Owner => Response::Owner(helpers::owner(ctx)),
+            _ => return Err(Error::BadRequest),
+        })
+    }
+}
+
+sdk::create_contract!(Ownable);
