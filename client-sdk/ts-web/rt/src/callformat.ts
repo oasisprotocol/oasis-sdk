084158faac820b15426c252fce5892f020e0d597
James Zheng <jameszheng@oasislabs.com>
2022-05-31 09:11:39 -0700

    fmt

--- client-sdk/ts-web/rt/src/callformat.ts
+++ client-sdk/ts-web/rt/src/callformat.ts
@@ -15,74 +15,91 @@ export const CALL_DATA_KEY_PAIR_ID_CONTEXT_BASE = 'oasis-runtime-sdk/private: tx
  *  EncodeConfig is call encoding configuration.
  */
 export interface EncodeConfig {
-  /**
-   * publicKey is an optional runtime's call data public key to use for encrypted call formats.
-   */
-  publicKey?: types.KeyManagerSignedPublicKey;
+    /**
+     * publicKey is an optional runtime's call data public key to use for encrypted call formats.
+     */
+    publicKey?: types.KeyManagerSignedPublicKey;
 }
 
 export interface metaEncryptedX25519DeoxysII {
-  sk: Uint8Array;
-  pk: Uint8Array;
+    sk: Uint8Array;
+    pk: Uint8Array;
 }
 
 /**
- * encodeCall encodes a call based on its configured call format. 
- * It returns the encoded call and any metadata needed to successfully decode the result.  
+ * encodeCall encodes a call based on its configured call format.
+ * It returns the encoded call and any metadata needed to successfully decode the result.
  */
-export async function encodeCall(call: types.Call, cf: types.CallFormat, cfg?: EncodeConfig): Promise<[types.Call, metaEncryptedX25519DeoxysII?]> {
-  switch (cf) {
-    case types.CALLFORMAT_PLAIN:
-      return [call, undefined];
-    case types.CALLFORMAT_ENCRYPTED_X25519DEOXYSII:
-      if (cfg && cfg.publicKey) {
-	const privateKey = ed.utils.randomPrivateKey();
-	const publicKey = ed.curve25519.scalarMultBase(privateKey)
-	const nonce = new Uint8Array(deoxysii.NonceSize);
-	crypto.getRandomValues(nonce);
-	const rawCall = oasis.misc.toCBOR(call);
-	const sealedCall = mrae.boxSeal(nonce, rawCall, null, cfg.publicKey.key, privateKey);
-	const envolope: types.CallEnvelopeX25519DeoxysII = {
-	  pk: publicKey,
-	  nonce: nonce,
-	  data: sealedCall,
-	}
-	const encoded: types.Call = {
-	  format: types.CALLFORMAT_ENCRYPTED_X25519DEOXYSII,
-	  method: "",
-	  body: oasis.misc.toCBOR(envolope),
-	};
-	const meta: metaEncryptedX25519DeoxysII = {
-	  sk: privateKey,
-	  pk: cfg.publicKey.key,
-	};
-	return [encoded, meta];
-      } else {
-	throw new Error('callformat: runtime call data public key not set');
-      }      
-  }
+export async function encodeCall(
+    call: types.Call,
+    cf: types.CallFormat,
+    cfg?: EncodeConfig,
+): Promise<[types.Call, metaEncryptedX25519DeoxysII?]> {
+    switch (cf) {
+        case types.CALLFORMAT_PLAIN:
+            return [call, undefined];
+        case types.CALLFORMAT_ENCRYPTED_X25519DEOXYSII:
+            if (cfg && cfg.publicKey) {
+                const privateKey = ed.utils.randomPrivateKey();
+                const publicKey = ed.curve25519.scalarMultBase(privateKey);
+                const nonce = new Uint8Array(deoxysii.NonceSize);
+                crypto.getRandomValues(nonce);
+                const rawCall = oasis.misc.toCBOR(call);
+                const sealedCall = mrae.boxSeal(
+                    nonce,
+                    rawCall,
+                    null,
+                    cfg.publicKey.key,
+                    privateKey,
+                );
+                const envolope: types.CallEnvelopeX25519DeoxysII = {
+                    pk: publicKey,
+                    nonce: nonce,
+                    data: sealedCall,
+                };
+                const encoded: types.Call = {
+                    format: types.CALLFORMAT_ENCRYPTED_X25519DEOXYSII,
+                    method: '',
+                    body: oasis.misc.toCBOR(envolope),
+                };
+                const meta: metaEncryptedX25519DeoxysII = {
+                    sk: privateKey,
+                    pk: cfg.publicKey.key,
+                };
+                return [encoded, meta];
+            } else {
+                throw new Error('callformat: runtime call data public key not set');
+            }
+    }
 }
 
 /**
  * decodeResult performs result decoding based on the specified call format metadata.
  */
 
-export function decodeResult(result: types.CallResult, meta?: metaEncryptedX25519DeoxysII): types.CallResult {
-  if (meta == undefined) {
-    /**
-     * In case of plain-text data format, we simply pass on the result unchanged.
-     */
-    return result;
-  } else {
-    if (result.unknown) {
-      const envelop = oasis.misc.fromCBOR(result.unknown) as types.ResultEnvelopeX25519DeoxysII;      
-      const pt = mrae.boxOpen(envelop.nonce, envelop.data, null, meta.pk, meta.sk);
-      const output = oasis.misc.fromCBOR(pt) as types.CallResult;
-      return output;
-    } else if (result.fail) {
-      throw new Error(`callformat: failed call: module :${result.fail.module} code: ${result.fail.code} message: ${result.fail.message}`);
+export function decodeResult(
+    result: types.CallResult,
+    meta?: metaEncryptedX25519DeoxysII,
+): types.CallResult {
+    if (meta == undefined) {
+        /**
+         * In case of plain-text data format, we simply pass on the result unchanged.
+         */
+        return result;
     } else {
-      throw new Error(`callformat: unexpected result: ${result.ok}`);
-    }    
-  }
+        if (result.unknown) {
+            const envelop = oasis.misc.fromCBOR(
+                result.unknown,
+            ) as types.ResultEnvelopeX25519DeoxysII;
+            const pt = mrae.boxOpen(envelop.nonce, envelop.data, null, meta.pk, meta.sk);
+            const output = oasis.misc.fromCBOR(pt) as types.CallResult;
+            return output;
+        } else if (result.fail) {
+            throw new Error(
+                `callformat: failed call: module :${result.fail.module} code: ${result.fail.code} message: ${result.fail.message}`,
+            );
+        } else {
+            throw new Error(`callformat: unexpected result: ${result.ok}`);
+        }
+    }
 }
--- client-sdk/ts-web/rt/src/mrae.ts
+++ client-sdk/ts-web/rt/src/mrae.ts
@@ -4,38 +4,50 @@ import {sha512_256} from 'js-sha512';
 
 var deoxysii = require('deoxysii');
 
-const BoxKDFTweak = "MRAE_Box_Deoxys-II-256-128";
+const BoxKDFTweak = 'MRAE_Box_Deoxys-II-256-128';
 
 /**
  * deriveSymmetricKey derives a MRAE AEAD symmetric key suitable for use with the asymmetric
- * box primitives from the provided X25519 public and private keys. 
+ * box primitives from the provided X25519 public and private keys.
  */
 
 export function deriveSymmetricKey(publicKey: Uint8Array, privateKey: Uint8Array): Uint8Array {
-  const pmk = ed.curve25519.scalarMult(oasis.misc.toHex(privateKey), oasis.misc.toHex(publicKey));
-  var kdf = sha512_256.hmac.create(BoxKDFTweak);
-  kdf.update(pmk);
-  return oasis.misc.fromHex(kdf.hex());
+    const pmk = ed.curve25519.scalarMult(oasis.misc.toHex(privateKey), oasis.misc.toHex(publicKey));
+    var kdf = sha512_256.hmac.create(BoxKDFTweak);
+    kdf.update(pmk);
+    return oasis.misc.fromHex(kdf.hex());
 }
 
 /**
- * boxSeal boxes ("seals") the provided additional data and plaintext via 
- * Deoxys-II-256-128 using a symmetric key derived from the provided 
- * X25519 public and private keys. 
+ * boxSeal boxes ("seals") the provided additional data and plaintext via
+ * Deoxys-II-256-128 using a symmetric key derived from the provided
+ * X25519 public and private keys.
  */
-export function boxSeal(nonce: Uint8Array, plainText: Uint8Array, associateData: Uint8Array, publicKey: Uint8Array, privateKey: Uint8Array): Uint8Array {
-  const sharedKey = deriveSymmetricKey(publicKey, privateKey);
-  var aead = new deoxysii.AEAD(sharedKey);
-  return aead.encrypt(nonce, plainText, associateData);
+export function boxSeal(
+    nonce: Uint8Array,
+    plainText: Uint8Array,
+    associateData: Uint8Array,
+    publicKey: Uint8Array,
+    privateKey: Uint8Array,
+): Uint8Array {
+    const sharedKey = deriveSymmetricKey(publicKey, privateKey);
+    var aead = new deoxysii.AEAD(sharedKey);
+    return aead.encrypt(nonce, plainText, associateData);
 }
 
 /**
- * boxOpen unboxes ("opens") the provided additional data and plaintext via 
- * Deoxys-II-256-128 using a symmetric key derived from the provided 
- * X25519 public and private keys. 
+ * boxOpen unboxes ("opens") the provided additional data and plaintext via
+ * Deoxys-II-256-128 using a symmetric key derived from the provided
+ * X25519 public and private keys.
  */
-export function boxOpen(nonce: Uint8Array, ciperText: Uint8Array, associateData: Uint8Array, publicKey: Uint8Array, privateKey: Uint8Array): Uint8Array {
-  const sharedKey = deriveSymmetricKey(publicKey, privateKey);
-  var aead = new deoxysii.AEAD(sharedKey);
-  return aead.decrypt(nonce, ciperText, associateData);
+export function boxOpen(
+    nonce: Uint8Array,
+    ciperText: Uint8Array,
+    associateData: Uint8Array,
+    publicKey: Uint8Array,
+    privateKey: Uint8Array,
+): Uint8Array {
+    const sharedKey = deriveSymmetricKey(publicKey, privateKey);
+    var aead = new deoxysii.AEAD(sharedKey);
+    return aead.decrypt(nonce, ciperText, associateData);
 }
