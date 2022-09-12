8f3347f4b580a6fa6657a5e7ef9440975b871b24
Matevž Jekovec <matevz@oasisprotocol.org>
2022-09-12 16:38:00 +0200

    docs: Migrate docs.oasis.dev -> docs.oasis.io

--- client-sdk/go/config/default.go
+++ client-sdk/go/config/default.go
@@ -5,7 +5,7 @@ var DefaultNetworks = Networks{
 	Default: "mainnet",
 	All: map[string]*Network{
 		// Mainnet network parameters.
-		// See https://docs.oasis.dev/general/oasis-network/network-parameters.
+		// See https://docs.oasis.io/node/mainnet.
 		"mainnet": {
 			ChainContext: "b11b369e0da5bb230b220127f5e7b242d385ef8c6f54906243f30af63c815535",
 			RPC:          "grpc.oasis.dev:443",
@@ -41,7 +41,7 @@ var DefaultNetworks = Networks{
 			},
 		},
 		// Oasis Protocol Foundation Testnet parameters.
-		// See https://docs.oasis.dev/general/foundation/testnet.
+		// See https://docs.oasis.io/node/testnet.
 		"testnet": {
 			ChainContext: "50304f98ddb656620ea817cc1446c401752a05a249b36c9b90dba4616829977a",
 			RPC:          "testnet.grpc.oasis.dev:443",
