1b9862d09c12565835c56f1e9ffb6be6f2c959e2
lukaw3d <lukajeran@oasislabs.com>
2023-05-09 22:30:59 +0200

    node18 with nagle's algorithm

--- client-sdk/ts-web/core/test/e2e-nodejs.js
+++ client-sdk/ts-web/core/test/e2e-nodejs.js
@@ -1,9 +1,14 @@
 // @ts-check
-const nock = require('nock')
+const nock = require('nock');
 nock.recorder.rec({
   output_objects: true
-})
-global.XMLHttpRequest = require('xhr2');
+});
+const xhr2 = require('xhr2');
+const http = require('http');
+global.XMLHttpRequest = xhr2;
+// Enable Nagle's algorithm
+xhr2.nodejsSet({ httpAgent: new http.Agent({ noDelay: false }) });
+
 const oasis = require('./..');
 
 import('../playground/src/startPlayground.mjs').then(({startPlayground}) => {
