9172351d95e9f3516f62eebbce156c172f5e5097
lukaw3d <lukajeran@oasislabs.com>
2023-05-09 22:41:21 +0200

    ipv6?

--- client-sdk/ts-web/core/playground/src/startPlayground.mjs
+++ client-sdk/ts-web/core/playground/src/startPlayground.mjs
@@ -2,7 +2,7 @@
 
 /** @param {import('./../..')} oasis */
 export async function startPlayground(oasis) {
-    const nic = new oasis.client.NodeInternal('http://localhost:42280');
+    const nic = new oasis.client.NodeInternal('http://[::1]:42280');
     // Try map with non-string keys.
     {
         const toAddr = 'oasis1qpl5634wyu6larn047he9af7a3qyhzx59u0mquw7';
