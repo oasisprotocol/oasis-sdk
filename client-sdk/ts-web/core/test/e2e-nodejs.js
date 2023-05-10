a6d1037f0ba00a94e0c0c47e88ca59c971fbda30
lukaw3d <lukajeran@oasislabs.com>
2023-05-11 01:51:58 +0200

    e2e-nodejs warn about nodejs version and flags

--- client-sdk/ts-web/core/test/e2e-nodejs.js
+++ client-sdk/ts-web/core/test/e2e-nodejs.js
@@ -1,5 +1,9 @@
 // @ts-check
 global.XMLHttpRequest = require('xhr2');
+if (typeof crypto === 'undefined') {
+    throw 'Upgrade to Node.js@>=19 or Node.js@>=16 with --experimental-global-webcrypto.'
+}
+
 const oasis = require('./..');
 
 import('../playground/src/startPlayground.mjs').then(async ({startPlayground}) => {
