168209fbd39b5b33edd239b975d54a82578f2230
Matevž Jekovec <matevz@oasisprotocol.org>
2024-09-12 18:51:21 +0200

    examples: Add musl target

--- examples/runtime-sdk/rofl-oracle/oracle/hardhat.config.ts
+++ examples/runtime-sdk/rofl-oracle/oracle/hardhat.config.ts
@@ -7,12 +7,26 @@ import "./tasks/query";
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
+    'sapphire': {
+      url: "https://sapphire.oasis.io",
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
+      chainId: 0x5afe,
+    },
     'sapphire-testnet': {
       url: "https://testnet.sapphire.oasis.io",
       accounts: process.env.PRIVATE_KEY
         ? [process.env.PRIVATE_KEY]
         : [],
       chainId: 0x5aff,
+    },
+    'sapphire-localnet': {
+      url: "http://localhost:8545",
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
+      chainId: 0x5afd,
     },
   },
 };
