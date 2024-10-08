8d4d2b86425259727e15d84133248154f18edeb2
Warren He <w@oasislabs.com>
2024-10-08 16:42:17 -0700

    ts-web/core: add WebCryptoSigner

--- client-sdk/ts-web/core/src/signature.ts
+++ client-sdk/ts-web/core/src/signature.ts
@@ -112,6 +112,86 @@ export class BlindContextSigner implements ContextSigner {
     }
 }
 
+export class WebCryptoSigner implements Signer {
+    privateCK: CryptoKey;
+    publicKey: Uint8Array;
+
+    constructor(privateCK: CryptoKey, publicKey: Uint8Array) {
+        this.privateCK = privateCK;
+        this.publicKey = publicKey;
+    }
+
+    /**
+     * Create a CryptoKeyPair from a 32-byte private key.
+     */
+    static async keyPairFromPrivateKey(privateKey: Uint8Array) {
+        const privateDER = misc.concat(
+            new Uint8Array([
+                // PrivateKeyInfo
+                0x30, 0x2e,
+                // version 0
+                0x02, 0x01, 0x00,
+                // privateKeyAlgorithm 1.3.101.112
+                0x30, 0x05, 0x06, 0x03, 0x2b, 0x65, 0x70,
+                // privateKey
+                0x04, 0x22, 0x04, 0x20,
+            ]),
+            privateKey,
+        );
+        const privateCK = await crypto.subtle.importKey('pkcs8', privateDER, {name: 'Ed25519'}, true, ['sign']);
+        const privateJWK = await crypto.subtle.exportKey('jwk', privateCK);
+        const publicJWK = {
+            kty: privateJWK.kty,
+            crv: privateJWK.crv,
+            x: privateJWK.x,
+        } as JsonWebKey;
+        const publicCK = await crypto.subtle.importKey('jwk', publicJWK, {name: 'Ed25519'}, true, ['verify']);
+        return {
+            publicKey: publicCK,
+            privateKey: privateCK,
+        } as CryptoKeyPair;
+    }
+
+    /**
+     * Get the public key from a CryptoKeyPair.
+     */
+    static async publicKeyFromKeyPair(keyPair: CryptoKeyPair) {
+        return new Uint8Array(await crypto.subtle.exportKey('raw', keyPair.publicKey));
+    }
+
+    /**
+     * Create an instance with a newly generated key.
+     */
+    static async generate(extractable: boolean) {
+        const keyPair = await crypto.subtle.generateKey({name: 'Ed25519'}, extractable, ['sign', 'verify']) as CryptoKeyPair;
+        return await WebCryptoSigner.fromKeyPair(keyPair);
+    }
+
+    /**
+     * Create an instance from a CryptoKeyPair.
+     */
+    static async fromKeyPair(keyPair: CryptoKeyPair) {
+        const publicKey = await WebCryptoSigner.publicKeyFromKeyPair(keyPair);
+        return new WebCryptoSigner(keyPair.privateKey, publicKey);
+    }
+
+    /**
+     * Create an instance from a 32-byte private key.
+     */
+    static async fromPrivateKey(privateKey: Uint8Array) {
+        const keyPair = await WebCryptoSigner.keyPairFromPrivateKey(privateKey);
+        return await WebCryptoSigner.fromKeyPair(keyPair);
+    }
+
+    public(): Uint8Array {
+        return this.publicKey;
+    }
+
+    async sign(message: Uint8Array): Promise<Uint8Array> {
+        return new Uint8Array(await crypto.subtle.sign({name: 'Ed25519'}, this.privateCK, message));
+    }
+}
+
 export type MessageHandlerBare<PARSED> = (v: PARSED) => void;
 export type MessageHandlersBare = {[context: string]: MessageHandlerBare<never>};
 export type MessageHandlerWithChainContext<PARSED> = (chainContext: string, v: PARSED) => void;
