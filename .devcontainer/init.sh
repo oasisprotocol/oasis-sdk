ce4664d8778c2c7990a629eeef2e676ae2b17847
Luka Jeran <lukaw3d@gmail.com>
2024-11-15 22:14:56 +0000

    auto install

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,8 +2,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 rustup show
 rustup target add x86_64-unknown-linux-musl
 sudo apt update #missing from docs
-sudo apt install musl-tools gcc-multilib clang
-sudo apt install pkg-config protobuf-compiler cmake #missing from docs
+sudo apt -y install musl-tools gcc-multilib clang
+sudo apt -y install pkg-config protobuf-compiler cmake #missing from docs
 cargo install fortanix-sgx-tools
 cargo install sgxs-tools
 
