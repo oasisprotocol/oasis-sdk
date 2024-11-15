0a3fa2578565ea3a27f66836f5885eef65bfe193
Luka Jeran <lukaw3d@gmail.com>
2024-11-15 21:13:30 +0000

    init

---
+++ .devcontainer/init.sh
@@ -0,0 +1,14 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup show
+rustup target add x86_64-unknown-linux-musl
+sudo apt update
+sudo apt install musl-tools gcc-multilib clang
+sudo apt install pkg-config protobuf-compiler cmake
+cargo install fortanix-sgx-tools
+cargo install sgxs-tools
+
+wget https://github.com/oasisprotocol/cli/releases/download/v0.10.2/oasis_cli_0.10.2_linux_amd64.tar.gz
+tar -zxvf oasis_cli_0.10.2_linux_amd64.tar.gz
+sudo mv ./oasis_cli_0.10.2_linux_amd64/oasis /usr/local/bin/oasis
+
+oasis rofl build sgx --mode unsafe
