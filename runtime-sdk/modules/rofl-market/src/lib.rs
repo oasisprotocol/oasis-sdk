20d13e9bc713441121af02321e3f023e5a4df170
ptrus <peter@u-s.si>
2025-05-15 15:41:36 +0200

    runtime-sdk/roflmarket: Emit InstanceUpdated event on InstanceClaimPayment

--- runtime-sdk/modules/rofl-market/src/lib.rs
+++ runtime-sdk/modules/rofl-market/src/lib.rs
@@ -836,6 +836,13 @@ impl<Cfg: Config> Module<Cfg> {
                 .claim(ctx, &provider, &mut instance)?;
             instance.updated_at = ctx.now();
             state::set_instance(instance);
+
+            CurrentState::with(|state| {
+                state.emit_event(Event::InstanceUpdated {
+                    provider: body.provider,
+                    id,
+                })
+            });
         }
 
         Ok(())
