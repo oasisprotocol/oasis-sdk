219d095bdb1e2d401b2cca53fdd643f57a344077
Jernej Kos <jernej@kos.mx>
2025-07-11 14:49:49 +0200

    runtime-sdk/modules/rofl: Add rofl.OriginApp internal call

--- runtime-sdk/src/modules/rofl/config.rs
+++ runtime-sdk/src/modules/rofl/config.rs
@@ -18,6 +18,8 @@ pub trait Config: 'static {
     const GAS_COST_CALL_AUTHORIZED_ORIGIN_NODE: u64 = 2000;
     /// Gas cost of rofl.AuthorizedOriginEntity call.
     const GAS_COST_CALL_AUTHORIZED_ORIGIN_ENTITY: u64 = 2000;
+    /// Gas cost of rofl.OriginApp call.
+    const GAS_COST_CALL_ORIGIN_APP: u64 = 1000;
     /// Gas cost of rofl.StakeThresholds call.
     const GAS_COST_CALL_STAKE_THRESHOLDS: u64 = 10;
     /// Gas cost of rofl.DeriveKey call.
